e8b67cae733a196363c803f32c2f5cbc50a5f850
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-02-08 13:00:06 +0300

    enhance auth, and adding some notes

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -3,6 +3,13 @@ const jwt = require('jsonwebtoken');
 const HttpError = require('../models/http-error');
 
 module.exports = (req, res, next) => {
+  // Its a browser behavior for creating HTTP request
+  // basically anything but get request, The browser automaticlly
+  // send 'OPTIONS' request before it send the actual request you
+  // went to send
+  if (req.method === 'OPTIONS') {
+    return next();
+  }
   try {
     const token = req.headers.authorization.split(' ')[1]; // {authorization: 'Bearer TOKEN'}
 
@@ -20,3 +27,11 @@ module.exports = (req, res, next) => {
 };
 
 //req.headers is automaticlly provided by 'express', it's a js object
+
+// NOTES about auth in front-end:
+// 1. The login api response with object contain {userId, username, role,token}
+// 2. We can implement a (login/logout) function depend on the token.
+// if there is token login, if not logout..
+// 3. We can use the token to (or role?!) to determine what should RENDER.
+// 4. it's important to save a token, for anothe requests.
+// 5. When sending a request, add an "authorization" and set it to `Bearer ${token}` to header of request.
