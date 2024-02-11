5f611bfe4bef5f6326a570abddabd20bf68a8e2b
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-02-11 09:35:39 +0300

    Cross-Origin Resource Sharing

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ const { unsupportedRoutes } = require('./middlewares/unsupported-routes');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
 app.use(express.json());
 
 app.use('/api/users', usersRouters);
